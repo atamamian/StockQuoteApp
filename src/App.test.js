42d71b10e4bf4841c5d2843d6333f6aae74359d2
Andre Tamamian <andotamamian@gmail.com>
2020-02-23 13:45:30 -0800

    Add testUtils script for indByTestAttr function | Initial setup for App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+
 import App from './App';
+import { findByTestAttr } from '../test/testUtils';
+
+const setup = () => {
+  return shallow(<App />)
+}
 
 describe('App Component', () => {
   test('should render without error', () => {
-    
+    const wrapper = setup();
+    expect(wrapper.exists()).toBe(true);
   });
 });
