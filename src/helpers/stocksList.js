93f5ad50ae54863101f11204039cb6a4c2460001
Andre Tamamian <andotamamian@gmail.com>
2020-02-23 20:01:43 -0800

    Add JSON list of sample stocks

---
+++ src/helpers/stocksList.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    stockName: 'Nike',
+    stockSymbol: 'NKE',
+    stockPrice: null
+  },
+  {
+    stockName: 'Apple',
+    stockSymbol: 'AAPL',
+    stockPrice: null
+  },
+  {
+    stockName: 'Intel',
+    stockSymbol: 'INTC',
+    stockPrice: null
+  },
+  {
+    stockName: 'Walmart',
+    stockSymbol: 'WMT',
+    stockPrice: null
+  },
+  {
+    stockName: 'Microsoft',
+    stockSymbol: 'MSFT',
+    stockPrice: null
+  },
+]
